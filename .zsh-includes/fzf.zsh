1cb4f07c04124e58583fb841cf270d823e8fb617
Kyle Johnson <github.com@kyle.engineer>
2025-09-28 18:12:17 -0700

    fix: guard fzf sourcing

--- .zsh-includes/fzf.zsh
+++ .zsh-includes/fzf.zsh
@@ -14,7 +14,14 @@ export FZF_DEFAULT_OPTS='
   --color info:144,prompt:161,spinner:135,pointer:135,marker:118
 '
 
-[[ $- == *i* ]] && source "/home/kljohnso/.fzf/shell/completion.zsh" 2> /dev/null
+if [[ $- == *i* ]]; then
+  if command -v fzf >/dev/null 2>&1; then
+    fzf_completion_file="${HOME}/.fzf/shell/completion.zsh"
+    [[ -f ${fzf_completion_file} ]] && source "${fzf_completion_file}" 2>/dev/null
+  else
+    print -u2 'warning: fzf not installed; skipping fzf shell integration.'
+  fi
+fi
 
 export FZF_COMPLETION_TRIGGER=''
 for keymap in emacs viins; do
