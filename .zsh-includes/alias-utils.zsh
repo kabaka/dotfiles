28e206760f964b3059c33c02cd925d580949badf
Kyle Johnson <kyle@vacantminded.com>
2025-09-29 02:12:22 +0000

    zsh/alias-utils: remove unused aliases, fix webserver

--- .zsh-includes/alias-utils.zsh
+++ .zsh-includes/alias-utils.zsh
@@ -1,26 +1,15 @@
 if [ $(uname) != 'Darwin' ]; then
   alias ls='ls --color=always'
 fi
-#alias sl='ls --color=always'
 alias ndu='ncdu'
 alias v='vim'
 alias vi='vim'
 alias svim='sudo vim'
-alias nodpms='xset -dpms;xset s off'
 alias sz='source ~/.zshrc'
-#alias webserver='python3 -m http.server 8383'
-alias webserver='sudo thttpd -D -r -d . -p 8000 -l /dev/stdout -u kyle'
-#alias intrace='/home/kabaka/software/intrace/intrace-1.4.3/intrace'
-#alias gc='/home/kabaka/software/gcalcli/gcalcli'
+alias webserver='python3 -m http.server'
 alias x="chmod +x"
-alias pc="~/projects/PayCalc/pc.rb"
-alias g='git'
-alias gp='~/bin/genpass.rb'
 alias t='~/software/timebook/t'
-#alias start-alpine='tmux new ~/scripts/alpine-notify.sh'
-alias ss='~/bin/ss.sh'
 alias mksh='echo "#!/bin/sh" >>'
-#alias grep='grep --color=auto'
 alias urldecode='python3 -c "import sys, urllib.parse as up; \
       print(up.unquote_plus(sys.argv[1]))"'
 alias urlencode='python3 -c "import sys, urllib.parse as up; \
